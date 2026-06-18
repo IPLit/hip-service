b63e1e3cb564510f479712d9948191bb61584fb9
shilpa <sparab@iplit.in>
2026-06-18 19:35:19 +0530

    DataFlowClient fixes

--- src/In.ProjectEKA.HipService/DataFlow/DataFlowClient.cs
+++ src/In.ProjectEKA.HipService/DataFlow/DataFlowClient.cs
@@ -130,8 +130,8 @@ namespace In.ProjectEKA.HipService.DataFlow
                     .ToList();
                 var dataNotificationRequest = new DataNotificationRequest(dataResponse.TransactionId,
                     DateTime.Now.ToUniversalTime().ToString(Common.Constants.DateTimeFormat),
-                    new Notifier(Type.HIP, hipId),
-                    new StatusNotification(sessionStatus, hipId, statusResponses),
+                    new Notifier(Type.HIP, gatewayConfiguration.ClientId),
+                    new StatusNotification(sessionStatus, gatewayConfiguration.ClientId, statusResponses),
                     consentId,
                     requestId);
                 await GetDataNotificationRequest(dataNotificationRequest, cmSuffix, correlationId).ConfigureAwait(false);
