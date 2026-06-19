5141701a99a98dbcf5a6b0f7459404055474f3b0
shilpa <sparab@iplit.in>
2026-06-19 10:16:31 +0530

    Data transfer with hipId

--- src/In.ProjectEKA.HipService/DataFlow/DataFlowClient.cs
+++ src/In.ProjectEKA.HipService/DataFlow/DataFlowClient.cs
@@ -130,8 +130,8 @@ namespace In.ProjectEKA.HipService.DataFlow
                     .ToList();
                 var dataNotificationRequest = new DataNotificationRequest(dataResponse.TransactionId,
                     DateTime.Now.ToUniversalTime().ToString(Common.Constants.DateTimeFormat),
-                    new Notifier(Type.HIP, gatewayConfiguration.ClientId),
-                    new StatusNotification(sessionStatus, gatewayConfiguration.ClientId, statusResponses),
+                    new Notifier(Type.HIU, gatewayConfiguration.ClientId),
+                    new StatusNotification(sessionStatus, hipId, statusResponses),
                     consentId,
                     requestId);
                 await GetDataNotificationRequest(dataNotificationRequest, cmSuffix, correlationId).ConfigureAwait(false);
